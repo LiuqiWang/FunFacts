162e72f44d02a7c87cb64d29e39dc3670d75e8b6
Liuqi Wang <dy37freedom@gmail.com>
2017-02-21 17:07:11 -0500

    Initial commit

---
+++ app/src/main/java/co/sycee/funfacts/MainActivity.java
@@ -0,0 +1,47 @@
+package co.sycee.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Assign the Views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.textView2);
+        mShowFactButton = (Button) findViewById(R.id.button);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+
+                // Update the screen with our dynamic fact
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+    }
+}
